f22e4c88427f0b5edae7a7b8a987b69b48e290e0
Jouva Moufette <jouva@moufette.com>
2015-07-24 08:33:02 -0400

    Change flash message template to use Foundation style alert

--- src/views/message.blade.php
+++ src/views/message.blade.php
@@ -2,10 +2,9 @@
     @if (Session::has('flash_notification.overlay'))
         @include('flash::modal', ['modalClass' => 'flash-modal', 'title' => Session::get('flash_notification.title'), 'body' => Session::get('flash_notification.message')])
     @else
-        <div class="alert alert-{{ Session::get('flash_notification.level') }}">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-
+        <div data-alert class="alert-box {{ Session::get('flash_notification.level') }}">
             {{ Session::get('flash_notification.message') }}
+            <a href="#" class="close">&times;</a>
         </div>
     @endif
 @endif
